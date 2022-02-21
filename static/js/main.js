f7416d2c811bfd99d47fa391741c80d884feee32
Olivier Creurer <39808162+oliviercreurer@users.noreply.github.com>
2022-02-21 15:50:56 -0500

    fix menu icons

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,17 @@
 
 
-
-//Airtable Credentials
-// var Airtable = require('airtable');
-// var base = new Airtable({apiKey: ''}).base('appb2lxHLQHKIk9iv');
-
 // Menu Logic
 const navLinks = document.getElementsByClassName("nav-item");
-console.log(navLinks)
-
-const svgNames = ['0_target', '1_package', '2_dollar-sign', '3_trending-up']
-console.log(svgNames[1])
-
-function addNavIcons() {
-  for (let i = 0; i < navLinks.length; i++) {
-    let icon = document.createElement("img");
-    icon.src = `/img/menuicons/${svgNames[i]}.svg`;
-    navLinks[i].prepend(icon)
-  }
-}
-
-<span class="icon larger light" aria-hidden="true">
-              {{ partialCached (print .Pre) .}}
-            </span>
+const navIcons = document.getElementsByClassName("menu-icon");
 
 function addActiveClass() {
-  const links = document.getElementsByClassName('nav-link')
-  const currentUrl = location.href
+  const links = document.getElementsByClassName('nav-item');
+  const currentUrl = location.href;
   for (const link of links) {
-      if (link.href === currentUrl) {
-          link.classList.add('active')
+      if (link.children[1].href === currentUrl) {
+          link.classList.toggle('active')
       }
   }
 }
 
-addNavIcons()
 addActiveClass()
